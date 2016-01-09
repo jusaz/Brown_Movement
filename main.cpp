274a2965351947669636700f00edb1473d6dd2b2
jusaz <azevedo.miguel.jose@gmail.com>
2016-01-09 22:50:04 +0000

    Box para os botões de configuração adicionados

--- main.cpp
+++ main.cpp
@@ -22,7 +22,8 @@ glong   win_ylen    = 600 ;
 int main(int argc, char *argv[])
 {
   //criar a janela
-  GtkWidget *window;
+  GtkWidget *window, *frame, *vbox;
+
   gtk_init (&argc, &argv);
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
@@ -30,9 +31,17 @@ int main(int argc, char *argv[])
   gtk_window_set_title (GTK_WINDOW(window), "Movimento Browniano");
   gtk_window_set_position (GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 
+  vbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_container_set_border_width (GTK_CONTAINER (vbox), 5);
+  gtk_container_add (GTK_CONTAINER (window), vbox);
+
+  frame = gtk_frame_new ("Configuração");
+  gtk_frame_set_label_align (GTK_FRAME(frame), 0.5, 0.5);
+  gtk_box_pack_start (GTK_BOX(vbox), frame, FALSE, TRUE, 3);
+
   g_signal_connect (G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-  gtk_widget_show (window);
+  gtk_widget_show_all(window);
   gtk_main ();
 
 }
