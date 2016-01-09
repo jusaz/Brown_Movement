2b12141a9e63e2fabc41d6006ffcc86913f2766a
jusaz <azevedo.miguel.jose@gmail.com>
2016-01-09 19:12:00 +0000

    create window 800x600

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*********************************************************
  *                                                       *
- *      gtk3 - Cairo: Movimento Brownianio               *
+ *      gtk3 - Cairo: Movimento Browniano               *
  *                                                       *
  *      GTK+ 3                                           *
  *      Versao: 1.0                                      *
@@ -14,13 +14,26 @@
 #include <gtk/gtk.h>
 #include <math.h>
 
-gchar  *winTitle    = "Gtk3-Cairo: Rectângulo e círculo a moverem-se numa janela" ;
+const gchar  *winTitle    = "Movimento Browniano" ;
 glong   win_xlen    = 800 ;
 glong   win_ylen    = 600 ;
 
 
 int main(int argc, char *argv[])
 {
-  //comment
+  //criar a janela
+  GtkWidget *window;
+  gtk_init (&argc, &argv);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size (GTK_WINDOW(window), win_xlen, win_ylen);
+  gtk_window_set_title (GTK_WINDOW(window), "Movimento Browniano");
+  gtk_window_set_position (GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+  g_signal_connect (G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_widget_show (window);
+  gtk_main ();
+
 }
 
