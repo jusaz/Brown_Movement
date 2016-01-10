0f2473ddf7fc586ac82519d4f182a78630ca5ea3
jusaz <azevedo.miguel.jose@gmail.com>
2016-01-10 00:21:26 +0000

    adicionados comentarios

--- main.cpp
+++ main.cpp
@@ -53,30 +53,37 @@ int main(int argc, char *argv[])
   gtk_widget_set_size_request (window, win_xlen - 50, win_ylen - 50);
   gtk_window_set_title (GTK_WINDOW(window), winTitle);
 
+  //criação da box que suporta a drawing area e a frame de configuração
   main_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
   gtk_container_set_border_width (GTK_CONTAINER (main_box), 5);
   gtk_container_add (GTK_CONTAINER (window), main_box);
 
+  //criação da frame para a drawing area
   frame = gtk_frame_new ("");
   gtk_frame_set_label_align (GTK_FRAME(frame), 0.5, 0.5);
   gtk_box_pack_start (GTK_BOX(main_box), frame, TRUE, TRUE, 3);
 
+  //criação da drawing area
   draw_area = gtk_drawing_area_new ();
   gtk_container_add (GTK_CONTAINER (frame), draw_area);
 
+  //criação da frame de configuração
   frame = gtk_frame_new (" Configuração ");
   gtk_frame_set_label_align (GTK_FRAME(frame), 0.5, 0.5);
   gtk_box_pack_start (GTK_BOX(main_box), frame, FALSE, TRUE, 3);
 
+  //criação da button_box
   button_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
   gtk_container_set_border_width (GTK_CONTAINER (button_box), 10);
   gtk_container_add (GTK_CONTAINER (frame), button_box);
 
+  //criação do botão reiniciar
   button = gtk_button_new_with_label ("Reiniciar");
   gtk_widget_set_size_request (button, 170, 20);
   g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(cb_restart), NULL);
   gtk_box_pack_end (GTK_BOX(button_box), button, FALSE, TRUE, 3);
 
+  //criação do botão parar
   button = gtk_button_new_with_label ("Parar");
   gtk_widget_set_size_request (button, 170, 20);
   g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(cb_stop_continue), NULL);
@@ -87,5 +94,6 @@ int main(int argc, char *argv[])
   gtk_widget_show_all(window);
   gtk_main ();
 
+  return 0;
 }
 
