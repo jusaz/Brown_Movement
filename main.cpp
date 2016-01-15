47c2958c432ee731d7649733af8f2baf6a3fc6e4
jusaz <azevedo.miguel.jose@gmail.com>
2016-01-15 01:45:40 +0000

    Main window completa

--- main.cpp
+++ main.cpp
@@ -40,87 +40,12 @@ cb_restart (GtkWidget *widget ,
   return false;
 }
 
-gboolean
-cb_options_clicked (GtkWidget *widget ,
-                    GtkWidget *win    )
-{
-  GtkWidget    *op_win, *vbox, *table, *label, *user, *real, *home, *host;
-  GtkWidget    *lbl1, *lbl2, *lbl3, *lbl4;
-  gint result;
-
-  glong   xy_len    = 400 ;
-
-  GtkDialogFlags flags = GtkDialogFlags(GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT);
-
-  op_win = gtk_dialog_new_with_buttons("Opções",
-                                       GTK_WINDOW(win),
-                                       flags,
-                                       GTK_STOCK_OK,
-                                       GTK_RESPONSE_OK,
-                                       GTK_STOCK_CANCEL,
-                                       GTK_RESPONSE_REJECT,
-                                       NULL);
-
-  gtk_widget_set_size_request (op_win, xy_len, xy_len);
-  g_signal_connect (G_OBJECT(op_win), "destroy", G_CALLBACK(gtk_widget_destroy), op_win);
-
-  /* Create four entries that will tell the user what data to enter. */
-  lbl1 = gtk_label_new ("User Name:");
-  lbl2 = gtk_label_new ("Real Name:");
-  lbl3 = gtk_label_new ("Home Dir:");
-  lbl4 = gtk_label_new ("Host Name:");
-  user = gtk_entry_new();
-  real = gtk_entry_new();
-  home = gtk_entry_new();
-  host = gtk_entry_new();
-
-  /* Retrieve the user's information for the default values. */
-  gtk_entry_set_text (GTK_ENTRY (user), g_get_user_name());
-  gtk_entry_set_text (GTK_ENTRY (real), g_get_real_name());
-  gtk_entry_set_text (GTK_ENTRY (home), g_get_home_dir());
-  gtk_entry_set_text (GTK_ENTRY (host), g_get_host_name());
-
-
-  table = gtk_table_new(4, 2, FALSE);
-  gtk_table_attach_defaults(GTK_TABLE(table), lbl1, 0, 1, 0, 1);
-  gtk_table_attach_defaults(GTK_TABLE(table), lbl2, 0, 1, 1, 2);
-  gtk_table_attach_defaults(GTK_TABLE(table), lbl3, 0, 1, 2, 3);
-  gtk_table_attach_defaults(GTK_TABLE(table), lbl4, 0, 1, 3, 4);
-
-  gtk_table_attach_defaults(GTK_TABLE(table), user, 1, 2, 0, 1);
-  gtk_table_attach_defaults(GTK_TABLE(table), real, 1, 2, 1, 2);
-  gtk_table_attach_defaults(GTK_TABLE(table), home, 1, 2, 2, 3);
-  gtk_table_attach_defaults(GTK_TABLE(table), host, 1, 2, 3, 4);
-
-  gtk_table_set_row_spacings(GTK_TABLE (table), 5);
-  gtk_table_set_col_spacings(GTK_TABLE (table), 5);
-  gtk_container_set_border_width(GTK_CONTAINER (table), 5);
-
-  vbox = gtk_dialog_get_content_area(GTK_DIALOG(op_win));
-  gtk_container_add (GTK_CONTAINER (vbox), table);
-
-  gtk_widget_show_all(op_win);
-
-  /* Run the dialog and output the data if the user clicks the OK button. */
-  result = gtk_dialog_run (GTK_DIALOG (op_win));
-
-  if (result == GTK_RESPONSE_OK)
-  {
-    g_print ("User Name: %s\n", gtk_entry_get_text (GTK_ENTRY (user)));
-    g_print ("Real Name: %s\n", gtk_entry_get_text (GTK_ENTRY (real)));
-    g_print ("Home Folder: %s\n", gtk_entry_get_text (GTK_ENTRY (home)));
-    g_print ("Host Name: %s\n", gtk_entry_get_text (GTK_ENTRY (host)));
-  }
-
-  gtk_widget_destroy (op_win);
-
-  return FALSE;
-}
-
 int main(int argc, char *argv[])
 {
   //criar a janela
   GtkWidget *window, *draw_area, *frame, *main_box, *button_box, *button;
+
+  GtkWidget *label, *spin_button, *check_box;
 
   gtk_init (&argc, &argv);
 
@@ -166,11 +91,71 @@ int main(int argc, char *argv[])
   g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(cb_stop_continue), NULL);
   gtk_box_pack_end (GTK_BOX(button_box), button, FALSE, TRUE, 3);
 
-  //criação do botão Opcões
-  button = gtk_button_new_with_label ("Opções");
-  gtk_widget_set_size_request (button, 170, 20);
-  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(cb_options_clicked), window);
-  gtk_box_pack_end (GTK_BOX(button_box), button, FALSE, TRUE, 3);
+  label = gtk_label_new("Bola Grande");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_CENTER);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 6);
+
+  label = gtk_label_new("Massa:");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_START);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 3);
+
+  spin_button = gtk_spin_button_new_with_range(1, 100, 1);
+  gtk_box_pack_start(GTK_BOX(button_box), spin_button, FALSE, TRUE, 0);
+
+  label = gtk_label_new("Velocidade:");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_START);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 3);
+
+  spin_button = gtk_spin_button_new_with_range(1, 10, 1);
+  gtk_box_pack_start(GTK_BOX(button_box), spin_button, FALSE, TRUE, 0);
+
+  label = gtk_label_new("Ver só Bola Grande:");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_START);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 3);
+
+  check_box = gtk_check_button_new();
+  gtk_box_pack_start(GTK_BOX(button_box), check_box, FALSE, TRUE, 0);
+
+  label = gtk_label_new("Visualizar Velocidade:");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_START);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 3);
+
+  check_box = gtk_check_button_new();
+  gtk_box_pack_start(GTK_BOX(button_box), check_box, FALSE, TRUE, 0);
+
+  label = gtk_label_new("Bolas Pequenas");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_CENTER);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 6);
+
+  label = gtk_label_new("Massa:");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_START);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 3);
+
+  spin_button = gtk_spin_button_new_with_range(1, 100, 1);
+  gtk_box_pack_start(GTK_BOX(button_box), spin_button, FALSE, TRUE, 0);
+
+  label = gtk_label_new("Nº Bolas:");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_START);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 3);
+
+  spin_button = gtk_spin_button_new_with_range(1, 30, 1);
+  gtk_box_pack_start(GTK_BOX(button_box), spin_button, FALSE, TRUE, 0);
+
+  label = gtk_label_new("Velocidade Inicial:");
+  gtk_widget_set_hexpand (label, FALSE);
+  gtk_widget_set_halign (label, GTK_ALIGN_START);
+  gtk_box_pack_start(GTK_BOX(button_box), label, FALSE, TRUE, 3);
+
+  spin_button = gtk_spin_button_new_with_range(1, 10, 1);
+  gtk_box_pack_start(GTK_BOX(button_box), spin_button, FALSE, TRUE, 0);
 
   g_signal_connect (G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
